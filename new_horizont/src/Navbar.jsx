260216aa09c5f7f12997e989a9a7019ad3305cb0
Luis Pizano <luispizano883@gmail.com>
2025-09-19 17:49:41 -0300

    Se agregar despegable en navbar

--- new_horizont/src/Navbar.jsx
+++ new_horizont/src/Navbar.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom"; // 👈 Importar Link
 import './Navbar.css';
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <nav>
             <div className="link-home">
@@ -13,8 +15,17 @@ const Navbar = () => {
                 <li><Link to="/Temas" className="link">Nuestros temas</Link></li>
                 <li><Link to="/registro" className="link">Registro</Link></li>
                 <li><Link to="/login" className="link">Login</Link></li>
-                <li><Link to="/cuentas" className="link">Cuentas</Link></li>
-                <li><Link to="/logout" className="link">Logout</Link></li>
+                
+                <li className="dropdown">
+                    <button onClick={() => setIsOpen(!isOpen)}>Mi cuenta {isOpen ? "▲" : "▼"}</button>
+                    { isOpen && (
+                        <ul>
+                            <li><Link to="/cuentas" className="link">Cuentas</Link></li>
+                            <li><Link to="/logout" className="link">Logout</Link></li>
+                        </ul>
+                    )}
+                </li>
+
 
                 <form action="#">
                     <div className="buscador">
